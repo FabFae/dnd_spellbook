f5c8e6f0622970331ae28048363f888884a3dd31
fabian <faerberfabian@googlamil.com>
2024-10-23 20:31:08 +0200

    added spellinfo popup

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
 document.getElementById('display_known').addEventListener('click', displayKnown)
-document.getElementById('display_all').addEventListener('click', displayAll)
-document.getElementById('known_lvl1').addEventListener('click', collapse)
+document.getElementById('display_all').addEventListener('click', displayInfo)
 
+
+const DNDAPI = 'https://www.dnd5eapi.co'
 
 
 let allSpells = []
 let knownSpells = []
 
-getSpellsFromAPI()
+getAllSpellsFromAPI()
 
-async function getSpellsFromAPI() {
-  const url = `https://www.dnd5eapi.co/api/spells`;
+
+
+async function getAllSpellsFromAPI() {
+  const url = `${DNDAPI}/api/spells`;
 
   try {
     const res = await fetch(url); // Warte, bis die API-Daten geladen sind
@@ -28,8 +31,21 @@ async function getSpellsFromAPI() {
   }
 }
 
+async function getSpellInfo(spellPath) {
+  const url = `${DNDAPI}${spellPath}`;
+  let spellInfo;
+  
+  try {
+    const res = await fetch(url); // Warte, bis die API-Daten geladen sind
+    const data = await res.json(); // Warte, bis die JSON-Daten geparst sind   
+    
+    spellInfo = data;  
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
 
-function addToKnown(name){}
+  return spellInfo;
+}
 
 function addAllSpells(){
   allSpells.forEach(element => {
@@ -40,7 +56,7 @@ function addAllSpells(){
     // Erstelle den Button und setze den Text auf "+"
     const button = document.createElement('button');
     button.textContent = '+';
-    button.id = element.index + '-button'
+    button.id = `add${element.index}Button`
   
     // Füge den Button vor den Text des li Elements hinzu
     row.appendChild(button);
@@ -50,19 +66,92 @@ function addAllSpells(){
     row.appendChild(spellName);
   
     button.addEventListener('click', function() {
-      console.log(`You clicked on ${element.name}`); 
-    });  
+      addSpellToKnownSpells(element);
+    });
   
-    // if (element.level === 1) document.getElementById('all_spells1').appendChild(row);
-    // if (element.level === 2) document.getElementById('all_spells2').appendChild(row);
-    // if (element.level === 3) document.getElementById('all_spells3').appendChild(row);
-    // if (element.level === 4) document.getElementById('all_spells4').appendChild(row);
-    // if (element.level === 5) document.getElementById('all_spells5').appendChild(row);
-    // if (element.level === 6) document.getElementById('all_spells6').appendChild(row);
-    
+    if (element.level === 1) document.getElementById('all_spells1').appendChild(row);
+    if (element.level === 2) document.getElementById('all_spells2').appendChild(row);
+    if (element.level === 3) document.getElementById('all_spells3').appendChild(row);
+    if (element.level === 4) document.getElementById('all_spells4').appendChild(row);
+    if (element.level === 5) document.getElementById('all_spells5').appendChild(row);
+    if (element.level === 6) document.getElementById('all_spells6').appendChild(row);    
   })
 }
 
+
+function addSpellToKnownSpells(spell) {
+  if (!spellALreadyKnown(spell)){
+    knownSpells.push(spell); 
+    console.log(`added ${spell.name} to known spells`);
+    updateKnownSpellsUl() 
+  }
+}
+
+function removeSpellFromKnownSpells (spell){
+  for (let i = 0; i < knownSpells.length; i++){
+    if(spell.name === knownSpells[i].name){
+      knownSpells.splice(i, 1)
+      console.log(`removed ${spell.name} from known spells`) 
+      updateKnownSpellsUl()     
+    }
+  }
+}
+
+function updateKnownSpellsUl(){
+  //clear all knowns spells ULs
+  document.getElementById('known_spells1').innerHTML = '';
+  document.getElementById('known_spells2').innerHTML = '';
+  document.getElementById('known_spells3').innerHTML = '';
+  document.getElementById('known_spells4').innerHTML = '';
+  document.getElementById('known_spells5').innerHTML = '';
+  document.getElementById('known_spells6').innerHTML = '';
+
+  for (let i = 0; i < knownSpells.length; i++){
+    const spell = knownSpells[i] 
+
+    // Erstelle ein neues li Element
+    const row = document.createElement('li');
+
+      
+    // Erstelle den Button und setze den Text auf "+"
+    const removeButton = document.createElement('button');
+    removeButton.textContent = '-';
+    removeButton.id = `remove${spell.index}Button`  
+  
+    // Füge den Button vor den Text des li Elements hinzu
+    row.appendChild(removeButton);
+    
+    removeButton.addEventListener('click', function() {
+      removeSpellFromKnownSpells(spell);
+    });
+
+        // Setze den Text des li Elements
+    const spellName = document.createTextNode(' ' + spell.name + ' ');
+    row.appendChild(spellName);  
+
+    //add info-button to li
+    const infoButton = document.createElement('Button')
+    infoButton.textContent = 'i'
+    infoButton.id = `${spell.name}InfoButton`
+    row.appendChild(infoButton)
+    infoButton.addEventListener('click', function(){
+      displayInfo(spell)
+    })
+    
+    if (spell.level === 1) document.getElementById('known_spells1').appendChild(row);
+    if (spell.level === 2) document.getElementById('known_spells2').appendChild(row);
+    if (spell.level === 3) document.getElementById('known_spells3').appendChild(row);
+    if (spell.level === 4) document.getElementById('known_spells4').appendChild(row);
+    if (spell.level === 5) document.getElementById('known_spells5').appendChild(row);
+    if (spell.level === 6) document.getElementById('known_spells6').appendChild(row);  
+  }
+}
+
+function spellALreadyKnown(sut){
+  let known = false
+  knownSpells.forEach(spell => (sut.name === spell.name) ? known = true : {} )
+  return known
+}
 
 
 function displayKnown(){
@@ -76,19 +165,31 @@ function displayAll(){
   console.log(allSpells)
 }
 
-function collapse(){
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
-  
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
-    });
-  }
+async function displayInfo(spell) {
+
+  const spellInfo = await getSpellInfo(spell.url);
+  console.log(spellInfo)
+
+  // Hole das Modal und die Textbox
+  var modal = document.getElementById("spellInfo")
+  document.getElementById("spellName").textContent = `${spell.name}`
+  document.getElementById("spellDescription").textContent = `${spellInfo.desc}`
+
+
+
+  // Öffne das Modal
+  modal.style.display = "block"
+
+  // Schließe das Modal, wenn der Benutzer auf das "x" klickt
+  var closeBtn = document.getElementsByClassName("close")[0];
+  closeBtn.onclick = function() {
+    modal.style.display = "none"
+  };
+
+  // Schließe das Modal, wenn der Benutzer außerhalb des Popups klickt
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none"
+    }
+  };
 }
